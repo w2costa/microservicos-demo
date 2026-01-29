3c477a1c8cadd1c2fbf8a98d44c3e622935e7d80
Wilson Wolf Costa <wilson.w.costa@gmail.com>
2026-01-29 16:46:43 -0300

    Alteração de Escalabilidade e Balancemento de Carga

--- produtos.js
+++ produtos.js
@@ -1,22 +1,25 @@
 // produtos.js
 const express = require('express');
+const os = require('os'); // <--- ADICIONAR ISTO
 const app = express();
 const PORT = 3000;
 
-// Dados simulados (num caso real, viriam de uma Base de Dados)
 const listaDeProdutos = [
     { id: 1, nome: 'Notebook Gamer', preco: 1500 },
     { id: 2, nome: 'Teclado Mecânico', preco: 100 },
     { id: 3, nome: 'Monitor 24pol', preco: 250 }
 ];
 
-// Rota para listar todos os produtos
 app.get('/produtos', (req, res) => {
-    console.log('Recebi um pedido de lista de produtos!');
-    res.json(listaDeProdutos);
+    console.log('Recebi um pedido!');
+    
+    // Vamos devolver também quem atendeu o pedido
+    res.json({
+        quem_respondeu: os.hostname(), // <--- O NOME DO POD
+        dados: listaDeProdutos
+    });
 });
 
-// Iniciar o servidor
 app.listen(PORT, () => {
     console.log(`Serviço de Produtos a rodar em http://localhost:${PORT}`);
 });
